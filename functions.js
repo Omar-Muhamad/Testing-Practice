9be9d45b340750956c0188181316bb9fd958037a
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2021-12-11 01:24:25 +0200

    Add the rest of tests

--- functions.js
+++ functions.js
@@ -1,3 +1,26 @@
-export const stringLength = (string) => string.length;
+export default class Calculator {
+  static add(num1,num2) { return num1+num2}
 
-console.log (`The string length is ${stringLength('omar')} characters`);
+  static subtract(num1, num2) { return num1-num2}
+
+  static divide(num1, num2) { return num1/num2}
+
+  static multiply (num1, num2) { return num1*num2}
+}
+
+export const stringLength = (string) => {
+  const length = string.length;
+  return (length >= 1 && length <= 10)? length: 'error';
+};
+
+export const  reverseString = (string) => {
+  const newString = string.split('').reverse().join('');
+  return newString;
+};
+
+export const capitalizeString = (string) => {
+  let newString = string.charAt(0).toUpperCase() + string.slice(1);
+  return newString;
+};
+
+console.log(capitalizeString('omar'));
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,47 @@
-import { stringLength } from "./functions.js";
+import Calculator, { stringLength, reverseString, capitalizeString } from "./functions.js";
 
-test('omar is string with 4 characters', () => {
-  expect(stringLength('omar')).toBe(4);
+describe('String Length', () => {
+  test('"omar" contains 4 characters', () => {
+    expect(stringLength('omar')).toBe(4);
+  });
+  
+  test('An empty string should return error', () => {
+    expect(stringLength('')).toBe('error');
+  });
+  
+  test('"Omar Muhammad" contains more than 10 characters Should return error', () => {
+    expect(stringLength('Omar Muhammad')).toBe('error');
+  });
 });
+
+describe('Reverse String', () => {
+  test('the revesed string of "omar" is "ramo"' , () => {
+    expect(reverseString('omar')).toBe('ramo');
+  });
+});
+
+describe('Captalize String', () => {
+  test('the first character capitalized string of "omar" is "Omar"' , () => {
+    expect(capitalizeString('omar')).toBe('Omar');
+  });
+});
+
+describe('Calculator', () => {
+  test('4 + 2 should be equal to 6', () => {
+    expect(Calculator.add(4, 2)).toBe(6)
+  });
+
+  test('4 - 2 should be equal to 2', () => {
+    expect(Calculator.subtract(4, 2)).toBe(2)
+  });
+
+  test('4 / 2 should be equal to 2', () => {
+    expect(Calculator.divide(4, 2)).toBe(2)
+  });
+  
+  test('4 * 2 should be equal to 8', () => {
+    expect(Calculator.multiply(4, 2)).toBe(8)
+  });
+});
+
+
